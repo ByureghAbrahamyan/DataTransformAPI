f32c096542d0b20864ff2e76da5a21f7e8f02937
ByureghAbrahamyan <160909274+ByureghAbrahamyan@users.noreply.github.com>
2024-03-13 13:42:25 +0400

    Add files via upload

---
+++ Project/script.js
@@ -0,0 +1,10 @@
+
+letpopup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
